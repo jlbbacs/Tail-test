d1960eb34429dd254a6a0162976855ac33d2ab78
unknown <al_bacs@yahoo.com>
2023-05-16 20:40:19 +0800

    modift nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,11 +12,11 @@ const handleNav = () => {
 
             <ul className='hidden md:flex'>
 
-                <li className='p-4'>Home</li>
-                <li className='p-4'>Company</li>
-                <li className='p-4'>Recources</li>
-                <li className='p-4'>About</li>
-                <li className='p-4'>Contact</li>
+                <li className='p-4'> <a href='#'>Home</a></li>
+                <li className='p-4'> <a href='#'>Company</a></li>
+                <li className='p-4'> <a href='#'>Resources</a></li>
+                <li className='p-4'> <a href='#'>About</a></li>
+                <li className='p-4'> <a href='#'>Contact</a></li>
             </ul>
             
             <div onClick={handleNav} className='block md:hidden'> 
@@ -24,7 +24,7 @@ const handleNav = () => {
               
             </div>
 
-            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full bordered border-gray-900x bg-[#bcc1bc] ease-in-out duration-300 text-black' : 'fixed left-[-100%] xl-hidden'}>
+            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full bordered border-gray-900x bg-[#bcc1bc] ease-in-out duration-300 text-black' : 'fixed left-[-100%]'}>
             <h1 className='w-full text-3xl font-bold text-[#101110] m-4'>Erza.com</h1> 
 
                 <ul className='uppercase pt-4'>    
