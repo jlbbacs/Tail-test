d789bc1015661b7ac2bacf44d4924937a547bdb7
unknown <al_bacs@yahoo.com>
2023-05-16 20:17:42 +0800

    first Commit

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='text-white'>
+        <div className='max-w=[800px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+            <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DARA ANALYTICS</p>
+            <h1 className='md:text-7xl sm:text-6xl font-bold md:py-6'>GROW WITH DATA</h1>
+            <div>
+                <p className='md:text-5xl sm:text-4xl text-xl font-bold'>Fast, Flexible Financing for </p>
+            </div>
+        </div>
+    
+    </div> 
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+
+const Navbar = () => {
+const [nav, setNav] = useState(false)
+const handleNav = () => {
+    setNav(!nav)
+}
+  return (
+    <div className='flex justify-between items-center h-24 max-w-[1240px]  text-white  mx-auto px-4'>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Hello bacs</h1>
+
+            <ul className='hidden md:flex'>
+
+                <li className='p-4'>Home</li>
+                <li className='p-4'>Company</li>
+                <li className='p-4'>Recources</li>
+                <li className='p-4'>About</li>
+                <li className='p-4'>Contact</li>
+            </ul>
+            
+            <div onClick={handleNav} className='block md:hidden'> 
+                {!nav ?  <AiOutlineClose size={20}/> :<AiOutlineMenu size ={20}/>}
+              
+            </div>
+
+            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full bordered border-gray-900x bg-[#bcc1bc] ease-in-out duration-300 text-black' : 'fixed left-[-100%]'}>
+            <h1 className='w-full text-3xl font-bold text-[#101110] m-4'>Hello bacs</h1> 
+
+                <ul className='uppercase pt-4'>    
+                    <li className='p-4 '>Home</li>
+                    <li className='p-4 border border-x-0 gray-600'>Company</li>
+                    <li className='p-4 border border-x-0 gray-600'>Recources</li>
+                    <li className='p-4 border border-x-0 gray-600'>About</li>
+                    <li className='p-4'>Contact</li>
+                </ul>       
+
+            </div>
+            
+    </div>
+  )
+}
+
+export default Navbar
